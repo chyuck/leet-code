73661aadd761ebb726e0b52f7afd969516130506
Andrey Sergeev <andrey@chyuck.com>
2019-06-01 23:34:05 -0400

    Added "Valid Parentheses" data structure

---
+++ src/main/java/solutions/ValidParentheses.java
@@ -0,0 +1,44 @@
+package solutions;
+
+import java.util.Stack;
+
+/**
+ * Solution for https://leetcode.com/problems/valid-parentheses problem with
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if (s == null) return false;
+        if (s.length() == 0) return true;
+
+        final Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            final char ch = s.charAt(i);
+
+            // push to stack all open parentheses
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+                continue;
+            }
+
+            // no open parentheses for closed one
+            if (stack.isEmpty()) return false;
+
+            // closed parentheses should match open
+            if (ch == ')' && stack.pop() != '(')
+                return false;
+
+            if (ch == ']' && stack.pop() != '[')
+                return false;
+
+            if (ch == '}' && stack.pop() != '{')
+                return false;
+        }
+
+        // check whether all open parentheses have closed ones
+        return stack.isEmpty();
+    }
+}
---
+++ src/test/java/solutions/ValidParenthesesTests.java
@@ -0,0 +1,62 @@
+package solutions;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ValidParenthesesTests {
+
+    @Test public void testExample1() {
+        // arrange
+        final var solution = new ValidParentheses();
+
+        // act
+        final var result = solution.isValid("()");
+
+        // assert
+        assertTrue(result);
+    }
+
+    @Test public void testExample2() {
+        // arrange
+        final var solution = new ValidParentheses();
+
+        // act
+        final var result = solution.isValid("()[]{}");
+
+        // assert
+        assertTrue(result);
+    }
+
+    @Test public void testExample3() {
+        // arrange
+        final var solution = new ValidParentheses();
+
+        // act
+        final var result = solution.isValid("(]");
+
+        // assert
+        assertFalse(result);
+    }
+
+    @Test public void testExample4() {
+        // arrange
+        final var solution = new ValidParentheses();
+
+        // act
+        final var result = solution.isValid("([)]");
+
+        // assert
+        assertFalse(result);
+    }
+
+    @Test public void testExample5() {
+        // arrange
+        final var solution = new ValidParentheses();
+
+        // act
+        final var result = solution.isValid("{[]}");
+
+        // assert
+        assertTrue(result);
+    }
+}
