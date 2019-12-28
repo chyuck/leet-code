d8a8b9acf436fa5aa9d51f2f2155a39f396e96b7
chyuck <andrey@chyuck.com>
2019-12-27 20:35:33 -0500

    Updated "Best time to buy and sell stocks"

--- src/test/java/solutions/BestTimeBuySellStockTests.java
+++ src/test/java/solutions/BestTimeBuySellStockTests.java
@@ -26,26 +26,4 @@ public class BestTimeBuySellStockTests {
         // assert
         assertEquals(0, result);
     }
-
-    @Test public void testExample1ForSolution2(){
-        // arrange
-        final var solution = new BestTimeBuySellStock();
-
-        // act
-        final var result = solution.maxProfit2(new int[] { 7,1,5,3,6,4 });
-
-        // assert
-        assertEquals(5, result);
-    }
-
-    @Test public void testExample2ForSolution2(){
-        // arrange
-        final var solution = new BestTimeBuySellStock();
-
-        // act
-        final var result = solution.maxProfit2(new int[] { 7,6,4,3,1 });
-
-        // assert
-        assertEquals(0, result);
-    }
 }
