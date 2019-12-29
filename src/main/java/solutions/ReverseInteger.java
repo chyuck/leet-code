d4d4eee816f62f3321d4f0f1449d28000564df8f
chyuck <andrey@chyuck.com>
2019-12-29 13:41:17 -0500

    Updated 'Reverse Integer'

--- src/main/java/solutions/ReverseInteger.java
+++ src/main/java/solutions/ReverseInteger.java
@@ -1,22 +1,39 @@
 package solutions;
 
+/**
+ * Solution for https://leetcode.com/problems/reverse-integer/ problem with
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ * where n - number of digits
+ */
 public class ReverseInteger {
-    public int reverse(int x) {
-        int result = 0;
 
+    public int reverse(int x) {
         int sign = x >= 0 ? 1 : -1;
-        x = Math.abs(x);
+        x *= sign;
+
+        int reversed = 0;
 
         while (x > 0) {
+
+            // check overflow case
+            if (x < 10) {
+                int maxWithoutLastDigit = Integer.MAX_VALUE / 10;
+                int maxLastDigit = Integer.MAX_VALUE % 10;
+
+                if (maxWithoutLastDigit < reversed) {
+                    return 0;
+                }
+                if (maxWithoutLastDigit == reversed && maxLastDigit < x) {
+                    return 0;
+                }
+            }
+
             int digit = x % 10;
-            x = x / 10;
-
-            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10))
-                return 0;
-
-            result = result * 10 + digit;
+            reversed = reversed * 10 + digit;
+            x /= 10;
         }
 
-        return result * sign;
+        return sign * reversed;
     }
 }
