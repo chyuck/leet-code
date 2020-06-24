d5d8af77e0127c9bfb3605e1c0d3b0267807197c
chyuck <andrey@chyuck.com>
2020-06-23 20:56:26 -0400

    Implemented createSolutionFile method

--- src/main/java/scripts/CreateNewSolution.java
+++ src/main/java/scripts/CreateNewSolution.java
@@ -1,6 +1,8 @@
 package scripts;
 
-import java.util.regex.Matcher;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
@@ -10,16 +12,20 @@ import com.google.common.base.Preconditions;
 public class CreateNewSolution {
 
     public static void main(String[] args) {
-        if (args == null || args.length != 2) {
-            throw new IllegalArgumentException("Arguments: number, url");
+        try {
+            if (args == null || args.length != 2) {
+                throw new IllegalArgumentException("Arguments: number, url");
+            }
+
+            createSolution(args[0], args[1]);
+
+            System.out.println("Solution files created");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        createSolution(args[0], args[1]);
-
-        System.out.println("Solution files created");
     }
 
-    private static void createSolution(String number, String url) {
+    private static void createSolution(String number, String url) throws IOException {
         Preconditions.checkArgument(StringUtils.isNotBlank(number));
         Preconditions.checkArgument(StringUtils.isNotBlank(url));
 
@@ -36,17 +42,17 @@ public class CreateNewSolution {
     static String createName(String url) {
         Preconditions.checkArgument(StringUtils.isNotBlank(url));
 
-        Matcher matcher = URL_PATTERN.matcher(url);
+        var matcher = URL_PATTERN.matcher(url);
         if (!matcher.find()) {
             throw new IllegalArgumentException("Invalid LeetCode URL.")    ;
         }
 
-        String urlNamePart = matcher.group(1);
-        String[] nameParts = urlNamePart.split("-");
+        var urlNamePart = matcher.group(1);
+        var nameParts = urlNamePart.split("-");
 
-        StringBuilder sb = new StringBuilder();
+        var sb = new StringBuilder();
 
-        for (String namePart : nameParts) {
+        for (var namePart : nameParts) {
             sb.append(Character.toUpperCase(namePart.charAt(0)));
             sb.append(namePart.substring(1));
         }
@@ -54,8 +60,33 @@ public class CreateNewSolution {
         return sb.toString();
     }
 
-    private static void createSolutionFile(String name, String url) {
+    private static void createSolutionFile(String name, String url) throws IOException {
+        Preconditions.checkArgument(StringUtils.isNotBlank(name));
+        Preconditions.checkArgument(StringUtils.isNotBlank(url));
 
+        var sb = new StringBuilder();
+
+        appendLine(sb, "package solutions;");
+        appendLine(sb, StringUtils.EMPTY);
+        appendLine(sb, "/**");
+        appendLine(sb, " * Solution for " + url +" problem with");
+        appendLine(sb, " * Time complexity: O(?)");
+        appendLine(sb, " * Space complexity: O(?)");
+        appendLine(sb, " */");
+        appendLine(sb, "public class " + name + " {");
+        appendLine(sb, StringUtils.EMPTY);
+        appendLine(sb, "}");
+
+        var bytes = sb.toString().getBytes();
+        var path = Paths.get("src", "main", "java", "solutions", name + ".java");
+        Files.write(path, bytes);
+    }
+
+    private static void appendLine(StringBuilder sb, String line) {
+        Preconditions.checkNotNull(sb);
+        Preconditions.checkNotNull(line);
+
+        sb.append(line).append(System.lineSeparator());
     }
 
     private static void createTestsFile(String name) {
