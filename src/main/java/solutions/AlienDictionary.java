537da5ff820d0c5c63ed36696297de054288c98e
chyuck <andrey@chyuck.com>
2020-06-26 19:38:51 -0400

    Updated "Alien Dictionary" solution

--- src/main/java/solutions/AlienDictionary.java
+++ src/main/java/solutions/AlienDictionary.java
@@ -2,85 +2,113 @@ package solutions;
 
 import java.util.*;
 
+/**
+ * Solution for https://leetcode.com/problems/alien-dictionary/ problem with
+ * Time complexity: O(c)
+ * Space complexity: O(1)
+ * where c - total number of characters in all words
+ */
 public class AlienDictionary {
 
+    class InvalidOrderException extends RuntimeException {}
+
     public String alienOrder(String[] words) {
-        if (words == null || words.length == 0) return "";
-
-        HashSet<Character> chars = new HashSet<>();
-        for (String word : words) {
-            for (char c : word.toCharArray()) {
-                if (!chars.contains(c))
-                    chars.add(c);
-            }
-        }
-
-        Map<Character, Set<Character>> graph = new HashMap<>();
-        for (int i = 0; i < words.length - 1; i++) {
-            int length = Math.min(words[i].length(), words[i+1].length());
-
-            for (int j = 0; j < length; j++) {
-                char c1 = words[i].charAt(j);
-                char c2 = words[i+1].charAt(j);
-
-                if (c1 == c2) continue;
-
-                Set<Character> list = graph.get(c1);
-                if (list == null) list = new HashSet<Character>();
-                if (!list.contains(c2)) list.add(c2);
-                graph.put(c1, list);
-                break;
-            }
-        }
-
-        Map<Character, Status> visitStatus = new HashMap<>(chars.size());
-        Stack<Character> results = new Stack<>();
-
         try {
-            for (Character c : chars) {
-                dfs(graph, visitStatus, results, c);
-            }
+            Map<Character, Set<Character>> graph = buildGraph(words);
+            Set<Character> chars = getAllChars(words);
+
+            return topologicalSort(chars, graph);
         } catch (InvalidOrderException e) {
             return "";
         }
-
-        StringBuilder sb = new StringBuilder();
-        while (results.size() > 0) {
-            sb.append(results.pop());
-        }
-
-        return sb.toString();
     }
 
-    enum Status { VISITING, VISITED; }
+    private Set<Character> getAllChars(String[] words) {
+        Set<Character> results = new HashSet<>();
 
-    class InvalidOrderException extends Exception {};
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                Character c = word.charAt(i);
+                results.add(c);
+            }
+        }
 
-    private void dfs(Map<Character, Set<Character>> graph, Map<Character, Status> visitStatus, Stack<Character> results, Character curr)
-            throws InvalidOrderException, UnsupportedOperationException {
-        if (visitStatus.containsKey(curr)) {
-            Status status = visitStatus.get(curr);
-            switch (status) {
-                case VISITING:
+        return results;
+    }
+
+    private Map<Character, Set<Character>> buildGraph(String[] words) {
+        Map<Character, Set<Character>> results = new HashMap<>();
+
+        for (int i = 1; i < words.length; i++) {
+            String word1 = words[i - 1];
+            String word2 = words[i];
+
+            int minLength = Math.min(word1.length(), word2.length());
+
+            for (int j = 0; j < minLength; j++) {
+                Character c1 = word1.charAt(j);
+                Character c2 = word2.charAt(j);
+
+                if (c1 != c2) {
+                    Set<Character> existingResult = results.get(c1);
+                    Set<Character> result = existingResult != null ? existingResult : new HashSet<>();
+
+                    result.add(c2);
+                    results.put(c1, result);
+                    break;
+                } else if (j == minLength - 1 && word1.length() > word2.length()) {
                     throw new InvalidOrderException();
-                case VISITED:
-                    return;
-                default:
-                    throw new UnsupportedOperationException();
+                }
             }
         }
 
-        visitStatus.put(curr, Status.VISITING);
+        return results;
+    }
 
-        Set<Character> children = graph.get(curr);
+    enum VisitStatus { VISITING, VISITED }
+
+    private String topologicalSort(Set<Character> chars, Map<Character, Set<Character>> graph) {
+        Stack<Character> results = new Stack<>();
+        Map<Character, VisitStatus> visited = new HashMap<>();
+
+        for (Character c : chars) {
+            dfp(c, graph, visited, results);
+        }
+
+        return convertStackToString(results);
+    }
+
+    private void dfp(Character c, Map<Character, Set<Character>> graph, Map<Character, VisitStatus> visited,
+        Stack<Character> results) {
+
+        VisitStatus status = visited.get(c);
+        if (status == null) {
+            visited.put(c, VisitStatus.VISITING);
+        } else if (status == VisitStatus.VISITED) {
+            return;
+        } else {
+            throw new InvalidOrderException();
+        }
+
+        Set<Character> children = graph.get(c);
         if (children != null) {
-            for (Character child: children) {
-                dfs(graph, visitStatus, results, child);
+            for (Character child : children) {
+                dfp(child, graph, visited, results);
             }
         }
 
-        visitStatus.put(curr, Status.VISITED);
+        results.push(c);
+        visited.put(c, VisitStatus.VISITED);
+    }
 
-        results.add(curr);
+    private String convertStackToString(Stack<Character> stack) {
+        StringBuilder result = new StringBuilder();
+
+        while (!stack.empty()) {
+            Character c = stack.pop();
+            result.append(c);
+        }
+
+        return result.toString();
     }
 }
--- src/test/java/solutions/AlienDictionaryTests.java
+++ src/test/java/solutions/AlienDictionaryTests.java
@@ -41,4 +41,64 @@ public class AlienDictionaryTests {
         // assert
         assertEquals("", result);
     }
+
+    @Test
+    public void testCase1(){
+        // arrange
+        var solution = new AlienDictionary();
+
+        // act
+        var result = solution.alienOrder(new String[] { "za","zb","ca","cb" });
+
+        // assert
+        assertEquals("zcab", result);
+    }
+
+    @Test
+    public void testCase2(){
+        // arrange
+        var solution = new AlienDictionary();
+
+        // act
+        var result = solution.alienOrder(new String[] { "abc", "ab" });
+
+        // assert
+        assertEquals("", result);
+    }
+
+    @Test
+    public void testCase3(){
+        // arrange
+        var solution = new AlienDictionary();
+
+        // act
+        var result = solution.alienOrder(new String[] { "z", "z" });
+
+        // assert
+        assertEquals("z", result);
+    }
+
+    @Test
+    public void testCase4(){
+        // arrange
+        var solution = new AlienDictionary();
+
+        // act
+        var result = solution.alienOrder(new String[] { "wrt","wrtkj" });
+
+        // assert
+        assertEquals("kjwtr", result);
+    }
+
+    @Test
+    public void testCase5(){
+        // arrange
+        var solution = new AlienDictionary();
+
+        // act
+        var result = solution.alienOrder(new String[] { "wnlb" });
+
+        // assert
+        assertEquals("nlwb", result);
+    }
 }
