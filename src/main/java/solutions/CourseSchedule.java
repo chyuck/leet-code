b56a761b91667cdfa9f2da08a14214af20d38343
chyuck <andrey@chyuck.com>
2020-06-27 21:50:35 -0400

    Updated "Course Schedule" solution

--- src/main/java/solutions/CourseSchedule.java
+++ src/main/java/solutions/CourseSchedule.java
@@ -2,46 +2,76 @@ package solutions;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+/**
+ * Solution for https://leetcode.com/problems/course-schedule/ problem with
+ * Time complexity: O(C + P)
+ * Space complexity: O(C + P)
+ * where C - number of courses, P - number of prerequisites
+ */
 public class CourseSchedule {
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if (prerequisites == null || prerequisites.length <= 1) return true;
-        if (numCourses == 0) return true;
+        Map<Integer, Set<Integer>> graph = buildGraph(prerequisites);
 
-        HashMap<Integer, Set<Integer>> graph = new HashMap<>();
-        for (int[] prerequisite : prerequisites) {
-            Set<Integer> children = graph.get(prerequisite[0]);
-            if (children == null) children = new HashSet<>();
-            children.add(prerequisite[1]);
-            graph.put(prerequisite[0], children);
+        return topologicalTraversal(numCourses, graph);
+    }
+
+    private Map<Integer, Set<Integer>> buildGraph(int[][] prerequisites) {
+        Map<Integer, Set<Integer>> results = new HashMap<>();
+
+        for (int[] edge : prerequisites) {
+            int course1 = edge[0];
+            int course2 = edge[1];
+
+            Set<Integer> existingResult = results.get(course1);
+            Set<Integer> result = existingResult != null ? existingResult : new HashSet<>();
+
+            result.add(course2);
+
+            results.put(course1, result);
         }
 
-        HashSet<Integer> visited = new HashSet<>();
+        return results;
+    }
 
-        for (Integer i=0; i < numCourses; i++) {
-            if (!dfs(graph, visited, i)) return false;
+    class InvalidOrderException extends RuntimeException {}
+
+    enum VisitStatus { VISITING, VISITED }
+
+    private boolean topologicalTraversal(int numCourses, Map<Integer, Set<Integer>> graph) {
+        Map<Integer, VisitStatus> visited = new HashMap<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            try {
+                dfs(i, graph, visited);
+            } catch (InvalidOrderException e) {
+                return false;
+            }
         }
 
         return true;
     }
 
-    private boolean dfs(HashMap<Integer, Set<Integer>> graph, HashSet<Integer> visited, Integer num) {
-        if (visited.contains(num))
-            return false;
+    private void dfs(int course, Map<Integer, Set<Integer>> graph, Map<Integer, VisitStatus> visited) {
+        VisitStatus status = visited.get(course);
+        if (status == null) {
+            visited.put(course, VisitStatus.VISITING);
+        } else if (status == VisitStatus.VISITED) {
+            return;
+        } else {
+            throw new InvalidOrderException();
+        }
 
-        visited.add(num);
-
-        Set<Integer> children = graph.get(num);
+        Set<Integer> children = graph.get(course);
         if (children != null) {
             for (Integer child : children) {
-                if (!dfs(graph, visited, child)) return false;
+                dfs(child, graph, visited);
             }
         }
 
-        visited.remove(num);
-
-        return true;
+        visited.put(course, VisitStatus.VISITED);
     }
 }
