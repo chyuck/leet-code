b31b2c912dcc7010b9644dc287ea1b993b12971b
chyuck <andrey@chyuck.com>
2020-06-30 20:02:38 -0400

    Added "Number of Connected Components in an Undirected Graph" solution

---
+++ src/main/java/solutions/NumberOfConnectedComponentsInAnUndirectedGraph.java
@@ -0,0 +1,78 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Solution for https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/ problem with
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+public class NumberOfConnectedComponentsInAnUndirectedGraph {
+
+    public int countComponents(int n, int[][] edges) {
+        Map<Integer, List<Integer>> graph = buildGraph(edges);
+
+        return getNumberOfConnectedComponents(n, graph);
+    }
+
+    private Map<Integer, List<Integer>> buildGraph(int[][] edges) {
+        Map<Integer, List<Integer>> results = new HashMap<>();
+
+        for (int[] edge : edges) {
+            addGraphEdge(results, edge[0], edge[1]);
+            addGraphEdge(results, edge[1], edge[0]);
+        }
+
+        return results;
+    }
+
+    private void addGraphEdge(Map<Integer, List<Integer>> graph, int a, int b) {
+        List<Integer> existingValues = graph.get(a);
+        List<Integer> values = existingValues != null ? existingValues : new ArrayList<>();
+
+        values.add(b);
+        graph.put(a, values);
+    }
+
+    private int getNumberOfConnectedComponents(int n, Map<Integer, List<Integer>> graph) {
+        int result = 0;
+
+        Set<Integer> visited = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!visited.contains(i)) {
+                dfs(i, graph, visited);
+                result++;
+            }
+
+            if (visited.size() == n) {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(int i, Map<Integer, List<Integer>> graph, Set<Integer> visited) {
+        if (visited.contains(i)) {
+            return;
+        }
+
+        visited.add(i);
+
+        List<Integer> values = graph.get(i);
+        if (values == null) {
+            return;
+        }
+
+        for (int value : values) {
+            dfs(value, graph, visited);
+        }
+    }
+
+}
---
+++ src/test/java/solutions/NumberOfConnectedComponentsInAnUndirectedGraphTests.java
@@ -0,0 +1,55 @@
+package solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NumberOfConnectedComponentsInAnUndirectedGraphTests {
+
+    @Test
+    public void testExample1(){
+        // arrange
+        var solution = new NumberOfConnectedComponentsInAnUndirectedGraph();
+
+        // act
+        var result = solution.countComponents(5, new int[][] { {0,1}, {1,2}, {3,4} });
+
+        // assert
+        Assert.assertEquals(2, result);
+    }
+
+    @Test
+    public void testExample2(){
+        // arrange
+        var solution = new NumberOfConnectedComponentsInAnUndirectedGraph();
+
+        // act
+        var result = solution.countComponents(5, new int[][] { {0,1}, {1,2}, {2,3}, {3,4} });
+
+        // assert
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void testCase1(){
+        // arrange
+        var solution = new NumberOfConnectedComponentsInAnUndirectedGraph();
+
+        // act
+        var result = solution.countComponents(2, new int[][] { {1,0} });
+
+        // assert
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void testCase2(){
+        // arrange
+        var solution = new NumberOfConnectedComponentsInAnUndirectedGraph();
+
+        // act
+        var result = solution.countComponents(3, new int[][] { {2,0}, {2,1} });
+
+        // assert
+        Assert.assertEquals(1, result);
+    }
+}
