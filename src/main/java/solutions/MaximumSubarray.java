25469e9caf641fb450113141c55f13c65fd829e5
chyuck <andrey@chyuck.com>
2020-07-09 16:19:56 -0400

    Updated "Maximum Subarray" solution

--- src/main/java/solutions/MaximumSubarray.java
+++ src/main/java/solutions/MaximumSubarray.java
@@ -7,29 +7,17 @@ package solutions;
  */
 public class MaximumSubarray {
 
-    /**
-     * Kadane's algorithm
-     * https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane's_algorithm
-     */
     public int maxSubArray(int[] nums) {
-        // max sum for all sub-arrays between 0 and i - 1 numbers
-        int previousMaxSum = nums[0];
-        // max sum among all max sums
-        int maxSum = previousMaxSum;
+        int result = Integer.MIN_VALUE;
+        int prevMax = 0;
 
-        // start from 2nd element, because 1st already used for setting initial max sums
-        for (int i = 1; i < nums.length; i++) {
-            // calculate max sum for all sub-arrays between 0 and i numbers
-            // it is maximum of:
-            //   - previous max sum + current number
-            //   - current number
-            int currentMaxSum = Math.max(previousMaxSum + nums[i], nums[i]);
-            // update result is current max is greater than all previous maxes
-            maxSum = Math.max(maxSum, currentMaxSum);
-            // update previous max sum for next iteration
-            previousMaxSum = currentMaxSum;
+        for (int num : nums) {
+            int max = Math.max(num, prevMax + num);
+            prevMax = max;
+
+            result = Math.max(result, max);
         }
 
-        return maxSum;
+        return result;
     }
 }
