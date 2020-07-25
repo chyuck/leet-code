e56d61c6f3122b3e53815fd1a1bd7a223c5618f4
chyuck <andrey@chyuck.com>
2020-07-24 21:11:56 -0400

    Added "Read N Characters Given Read4"

---
+++ src/main/java/solutions/ReadNCharactersGivenRead4.java
@@ -0,0 +1,56 @@
+package solutions;
+
+/**
+ * Solution for https://leetcode.com/problems/read-n-characters-given-read4/ problem with
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ */
+public class ReadNCharactersGivenRead4 extends Reader4 {
+
+    public int read(char[] buf, int n) {
+        int result = 0;
+        char[] buf4 = new char[4];
+
+        while (true) {
+            int result4 = read4(buf4);
+
+            for (int i = 0; i < result4 && result < n; i++) {
+                buf[result] = buf4[i];
+                result++;
+            }
+
+            if (result4 < 4 || result == n) {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public ReadNCharactersGivenRead4(String file) {
+        super(file);
+    }
+}
+
+class Reader4 {
+    private final String file;
+    private int pointer = 0;
+
+    public Reader4(String file) {
+        this.file = file;
+    }
+
+    int read4(char[] buf4) {
+        int result = 0;
+
+        for (int i = 0; i < 4 && pointer < file.length(); i++) {
+            char c = file.charAt(pointer);
+            buf4[i] = c;
+
+            pointer++;
+            result++;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/solutions/ReadNCharactersGivenRead4Tests.java
@@ -0,0 +1,67 @@
+package solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReadNCharactersGivenRead4Tests {
+
+    @Test
+    public void testExample1(){
+        // arrange
+        var solution = new ReadNCharactersGivenRead4("abc");
+
+        char[] buf = new char[3];
+
+        // act
+        var result = solution.read(buf, 4);
+
+        // assert
+        Assert.assertEquals(3, result);
+        Assert.assertEquals("abc", new String(buf));
+    }
+
+    @Test
+    public void testExample2(){
+        // arrange
+        var solution = new ReadNCharactersGivenRead4("abcde");
+
+        char[] buf = new char[5];
+
+        // act
+        var result = solution.read(buf, 5);
+
+        // assert
+        Assert.assertEquals(5, result);
+        Assert.assertEquals("abcde", new String(buf));
+    }
+
+    @Test
+    public void testExample3(){
+        // arrange
+        var solution = new ReadNCharactersGivenRead4("abcdABCD1234");
+
+        char[] buf = new char[12];
+
+        // act
+        var result = solution.read(buf, 12);
+
+        // assert
+        Assert.assertEquals(12, result);
+        Assert.assertEquals("abcdABCD1234", new String(buf));
+    }
+
+    @Test
+    public void testExample4(){
+        // arrange
+        var solution = new ReadNCharactersGivenRead4("leetcode");
+
+        char[] buf = new char[5];
+
+        // act
+        var result = solution.read(buf, 5);
+
+        // assert
+        Assert.assertEquals(5, result);
+        Assert.assertEquals("leetc", new String(buf));
+    }
+}
