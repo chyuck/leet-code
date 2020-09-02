72df57a9e5af6845eec4a12ecac8f48f84d89dcc
chyuck <andrey@chyuck.com>
2020-09-01 21:58:24 -0400

    Updated "Basic Calculator"

--- src/main/java/solutions/BasicCalculator.java
+++ src/main/java/solutions/BasicCalculator.java
@@ -8,62 +8,54 @@ package solutions;
 public class BasicCalculator {
 
     public int calculate(String s) {
-        String expressionWithoutSpaces = removeSpaces(s);
-
-        return calculate(expressionWithoutSpaces, new int[] { 0 });
+        return calculate(s, new int[] { 0 });
     }
 
-    private static String removeSpaces(String s) {
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (c != ' ') {
-                result.append(c);
-            }
-        }
-
-        return result.toString();
-    }
-
-    private static int calculate(String exp, int[] index) {
+    private static int calculate(String s, int[] index) {
         int result = 0;
 
-        int sign = 1;
+        int number = 0;
 
-        while (index[0] < exp.length()) {
-            char c = exp.charAt(index[0]);
+        char lastSign = '+';
+        int subResult = 0;
+
+        while (index[0] < s.length()) {
+            char c = s.charAt(index[0]);
 
             if (Character.isDigit(c)) {
-                int number = getNumber(exp, index);
-                result += sign * number;
-            } else if (c == '+') {
-                sign = 1;
-                index[0]++;
-            } else if (c == '-') {
-                sign = -1;
-                index[0]++;
+                int digit = c - '0';
+                number = number * 10 + digit;
+
             } else if (c == '(') {
                 index[0]++;
-                result += sign * calculate(exp, index);
-            } else if (c == ')') {
-                index[0]++;
-                return result;
+                number = calculate(s, index);
             }
-        }
 
-        return result;
-    }
+            if (c == '+' || c == '-' || c == ')' || index[0] == s.length() - 1) {
+                switch (lastSign) {
+                    case '+':
+                        result += subResult;
+                        subResult = number;
+                        break;
+                    case '-':
+                        result += subResult;
+                        subResult = -number;
+                        break;
+                }
 
-    private static int getNumber(String exp, int[] index) {
-        int number = 0;
-        while (index[0] < exp.length() && Character.isDigit(exp.charAt(index[0]))) {
-            int digit = exp.charAt(index[0]) - '0';
-            number = number * 10 + digit;
+                if (c == ')') {
+                    break;
+                }
+
+                lastSign = c;
+                number = 0;
+            }
+
             index[0]++;
         }
 
-        return number;
+        result += subResult;
+
+        return result;
     }
 }
