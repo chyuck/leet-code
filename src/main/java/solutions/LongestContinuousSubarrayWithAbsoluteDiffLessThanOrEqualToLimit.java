0ef26479cf19b9be8ea828ed949d6eb992b36952
chyuck <andrey@chyuck.com>
2020-10-03 14:59:16 -0400

    Added "Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit"

---
+++ src/main/java/solutions/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
@@ -0,0 +1,49 @@
+package solutions;
+
+import java.util.LinkedList;
+
+/**
+ * Solution for https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/ problem with
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+public class LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit {
+
+    public int longestSubarray(int[] nums, int limit) {
+        LinkedList<Integer> minNums = new LinkedList<>(), maxNums = new LinkedList<>();
+
+        int result = 0;
+
+        int left = 0, right = 0;
+
+        while (right < nums.length) {
+            while (!minNums.isEmpty() && minNums.getLast() > nums[right]) {
+                minNums.removeLast();
+            }
+            minNums.addLast(nums[right]);
+
+            while (!maxNums.isEmpty() && maxNums.getLast() < nums[right]) {
+                maxNums.removeLast();
+            }
+            maxNums.addLast(nums[right]);
+
+            while (maxNums.getFirst() - minNums.getFirst() > limit) {
+                if (minNums.getFirst() == nums[left]) {
+                    minNums.removeFirst();
+                }
+
+                if (maxNums.getFirst() == nums[left]) {
+                    maxNums.removeFirst();
+                }
+
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+
+            right++;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/solutions/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimitTests.java
@@ -0,0 +1,43 @@
+package solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimitTests {
+
+    @Test
+    public void testExample1() {
+        // arrange
+        var solution = new LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit();
+
+        // act
+        var result = solution.longestSubarray(new int[] { 8,2,4,7 }, 4);
+
+        // assert
+        Assert.assertEquals(2, result);
+    }
+
+    @Test
+    public void testExample2() {
+        // arrange
+        var solution = new LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit();
+
+        // act
+        var result = solution.longestSubarray(new int[] { 10,1,2,4,7,2 }, 5);
+
+        // assert
+        Assert.assertEquals(4, result);
+    }
+
+    @Test
+    public void testExample3() {
+        // arrange
+        var solution = new LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit();
+
+        // act
+        var result = solution.longestSubarray(new int[] { 4,2,2,2,4,4,2,2 }, 0);
+
+        // assert
+        Assert.assertEquals(3, result);
+    }
+}
